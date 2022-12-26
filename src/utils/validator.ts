7f81357b57a6ddd51efcd5a7de72b7b1b7c77d8b
AryanTy20 <devty007@gmail.com>
2022-12-26 11:16:59 +0530

    fix: :bug: confirm_password required

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -6,7 +6,7 @@ export const registerValidator = joi.object({
   last_name: joi.string().min(3).max(20).required(),
   email: joi.string().min(3).email().required(),
   password: joi.string().min(6).max(20).required(),
-  confirm_password: joi.string().valid(joi.ref("password")),
+  confirm_password: joi.string().valid(joi.ref("password")).required(),
   role: joi.string().valid("admin", "user").required(),
   department: joi.string().max(50),
 });
