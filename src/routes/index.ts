91ecf56146ca3090897118ad6eb0fb6d6e7b7ea9
AryanTy20 <devty007@gmail.com>
2022-12-26 21:19:21 +0530

    feat: refresh route created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,4 +10,7 @@ router.post("/login", Controller.login);
 /* This is a route that will be used to logout a user. */
 router.get("/logout", Controller.logout);
 
+/* This is a route that will be used to refresh a user's token. */
+router.get("/refresh", Controller.refresh);
+
 export default router;
