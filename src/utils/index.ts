7fc9a182b88f9171899793bbc677937b0250729f
AryanTy20 <devty007@gmail.com>
2022-12-26 10:18:33 +0530

    chore: indexed export added for utils folder

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+import { DbConnect } from "./Database";
+import { CustomError, ExtendedError } from "./error";
+import { JWTService } from "./jwt";
+
+export { CustomError, DbConnect, ExtendedError, JWTService };
