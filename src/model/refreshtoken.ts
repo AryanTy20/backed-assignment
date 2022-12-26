80b2136102c3eabfb1f2e4610b07325aa7ec3ee5
AryanTy20 <devty007@gmail.com>
2022-12-26 13:51:59 +0530

    feat: refreshToken schema created

--- src/model/refreshtoken.ts
+++ src/model/refreshtoken.ts
@@ -1,15 +1,10 @@
 import mongoose from "mongoose";
 
 const refreshSchema = new mongoose.Schema({
-  id: {
-    type: mongoose.SchemaTypes.ObjectId,
-    required: true,
-    unique: true,
-  },
-  refreshToken: {
+  token: {
     type: String,
     required: true,
   },
 });
 
-export default mongoose.model("RefreshToken", refreshSchema);
+export default mongoose.model("Refreshtoken", refreshSchema);
