ea980cb16dbfabd30f51d52718733710b14ad699
AryanTy20 <devty007@gmail.com>
2022-12-26 12:14:50 +0530

    feat: admin register controller added

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -1,4 +1,34 @@
 import { NextFunction, Request, Response } from "express";
-import { CustomError, registerValidator } from "../utils";
+import {
+  CustomError,
+  registerValidator,
+  hashedPassword,
+  JWTService,
+} from "../utils";
+import UserModel from "../model";
 
-export const AdminController = {};
+export const AdminController = {
+  async register(req: Request, res: Response, next: NextFunction) {
+    const { error } = registerValidator.validate(req.body);
+    if (error) return next(CustomError(422, error.message));
+    const { password, confirm_password, ...rest } = req.body;
+    try {
+      const hash = hashedPassword(password);
+      const user = await UserModel.create({ ...rest, password: hash });
+      const token = JWTService.sign({ _id: String(user._id), role: user.role });
+      const refreshToken = JWTService.refresh({
+        _id: String(user._id),
+        role: user.role,
+      });
+      res.cookie("refresh", refreshToken, {
+        maxAge: 604800000,
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      });
+      res.status(201).json({ user: user.first_name, token });
+    } catch (err) {
+      return next(err);
+    }
+  },
+};
