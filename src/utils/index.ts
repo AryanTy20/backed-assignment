152a9f95beef797dc8f509d941341749a83686db
AryanTy20 <devty007@gmail.com>
2022-12-26 11:22:06 +0530

    chore: :art: indexed export of passwordEncrypt

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,13 @@ import { DbConnect } from "./Database";
 import { CustomError, ExtendedError } from "./error";
 import { JWTService } from "./jwt";
 import { registerValidator } from "./validator";
+import { hashedPassword } from "./passwordEncrypt";
 
-export { CustomError, DbConnect, ExtendedError, JWTService, registerValidator };
+export {
+  CustomError,
+  DbConnect,
+  ExtendedError,
+  JWTService,
+  registerValidator,
+  hashedPassword,
+};
