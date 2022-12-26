8ea388f62720388867dde5270a359f5b19f2967a
AryanTy20 <devty007@gmail.com>
2022-12-26 09:35:38 +0530

    feat: Error handler added

--- src/server.ts
+++ src/server.ts
@@ -16,4 +16,14 @@ app.get("/", (req: Request, res: Response, next: NextFunction) =>
   res.json("api running 🚀")
 );
 
+// * Route for Error Handling *//
+import { ExtendedError } from "./utils/error";
+app.use(
+  (err: ExtendedError, req: Request, res: Response, next: NextFunction) => {
+    const status = err.status || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(status).json(message);
+  }
+);
+
 app.listen(5000, () => console.log("server running on port 5000"));
