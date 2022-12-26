610f8bed52df162892d1cef14e6d4235ebac9d2c
AryanTy20 <devty007@gmail.com>
2022-12-26 21:37:57 +0530

    docs: :memo: documentation added

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+/* Exporting the values of the variables DB_URL, SIGN_TOKEN, and REFRESH_TOKEN from the process.env
+file. */
 export const { DB_URL, SIGN_TOKEN, REFRESH_TOKEN } = process.env;
