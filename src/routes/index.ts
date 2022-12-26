33e8333b0e8181a6e2a2d5862a0b21fa69048361
AryanTy20 <devty007@gmail.com>
2022-12-26 21:26:26 +0530

    chore: :recycle: auth middleware added to logout and refresh

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 const router = Router();
 import { Controller } from "../controllers";
+import { Auth } from "../middleware";
 
 router.post("/register", Controller.register);
 
@@ -8,9 +9,9 @@ router.post("/register", Controller.register);
 router.post("/login", Controller.login);
 
 /* This is a route that will be used to logout a user. */
-router.get("/logout", Controller.logout);
+router.get("/logout", Auth, Controller.logout);
 
 /* This is a route that will be used to refresh a user's token. */
-router.get("/refresh", Controller.refresh);
+router.get("/refresh", Auth, Controller.refresh);
 
 export default router;
